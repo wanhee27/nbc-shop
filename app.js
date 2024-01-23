73182f575775af75d43ac7d4647931c27b8968ac
wanhee27 <chowh727@gmail.com>
2024-01-23 19:05:39 +0900

    .env 못읽어오던거 수정

--- app.js
+++ app.js
@@ -7,11 +7,9 @@ dotenv.config();
 
 const app = express();
 const PORT = 3000;
-
+connect();
 // Express에서 req.body에 접근하여 body 데이터를 사용할 수 있도록 설정합니다.
 app.use(express.json());
-
-connect();
 
 app.use('/api', productsRouter);
 
